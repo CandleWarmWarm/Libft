8a4e0c1e15f1eed1615c0b3bd05ddc6dcd0436bd
unknown <sunsola21259@gmail.com>
2026-08-29 00:25:59 +0700

    add

--- libft.h
+++ libft.h
@@ -53,4 +53,13 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 
+////
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+////
+
 #endif
